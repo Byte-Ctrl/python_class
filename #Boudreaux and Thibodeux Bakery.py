c1fb86479ef7f68453a6e8c33ccfc4c381fa5156
Byte-Ctrl <sarahtreadway1@dcc.edu>
2025-10-31 12:09:55 -0500

    Add files via upload

---
+++ #Boudreaux and Thibodeux Bakery.py
@@ -0,0 +1,10 @@
+#Boudreaux and Thibodeux Bakery
+ # A small program to take customer orders and calculate totals
+
+#menu and pricing
+items = ["Muffin", "King Cake Slice", "Croissant", "Scone"]
+prices = [5.95, 4.95, 3.95, 3.75]
+tax_rate = 0.0945
+
+#this is where i got to when the timer timed out due to it hitting 12pm 
+
